e2d28e50785a7a3dcff941ce79c324306ffd42a9
Bert Muthalaly <bert@somethingdoneright.net>
2014-11-16 13:30:15 -0500

    Scraper works

--- tim.py
+++ tim.py
@@ -1,33 +1,26 @@
 import requests
 from julienne import Julienne
 from BeautifulSoup import BeautifulSoup
-# http://www.basketball-reference.com/players/d/duranke01/gamelog/2014/
+import pickle
+
+# http://www.basketball-reference.com/players/d/duranke01/gamelog/2015/
 
 # for each NBA team
+# TODO: add TS% back
+
 def scrape():
-	for each_team_url in all_nba_teams():
-		# TODO: we really want both the player's name and URL inside here
-		for nba_player_gamelog_url in all_nba_players_gamelog_urls(each_team_url):
-			# visit this url
-			soup = BeautifulSoup(requests.get(nba_player_gamelog_url).text)
-			print(nba_player_gamelog_url)
-			player_init_table = soup.find(id="pgl_basic").prettify()
-			player_table = Julienne(player_init_table).select(columns=['3P', '3P%', 'PTS', 'TRB', 'AST', 'MP'])
-			print player_table
-			return player_table
-                        # advanced_table_soup = soup.find(id="pgl_advanced").prettify()
-			# player_advanced_table = Julienne(advanced_table_soup).select(columns=['TS%'])
-			# player_table.rows()
-			
-			# game_records_with_ts_percentage_added = []
-			# for game_record in player_table.rows():
-				# for advanced_game_record in player_advanced_table.rows():
-					# game_records_with_ts_percentage_added.append(game_record.update(advanced_game_record))
-			# now we have a collection of game records for a given player with all the stats we want
-			# [{PTS: 31, TS%: 65}, {}, {}
+    map_from_team_to_player_game_stats = {}
+    for each_team_name, each_team_url in all_nba_teams():
+        map_from_team_to_player_game_stats[each_team_name] = {}
+        for nba_player_name, nba_player_gamelog_url in all_nba_players_gamelog_urls(each_team_url):
+            soup = BeautifulSoup(requests.get(nba_player_gamelog_url).text)
+            player_init_table = soup.find(id="pgl_basic").prettify()
+            player_table = Julienne(player_init_table).select(columns=['3P', '3P%', 'PTS', 'TRB', 'AST', 'MP'])
+            map_from_team_to_player_game_stats[each_team_name][nba_player_name] = player_table
+
+    return map_from_team_to_player_game_stats
 
 def transform_scraped_data(map_from_team_to_player_game_stats):
-    # STATS = ['3P', '3P%', 'PTS', 'TRB', 'AST', 'TS%']
     STATS = ['3P', '3P%', 'PTS', 'TRB', 'AST', 'MP']
 
     result = {}
@@ -53,16 +46,17 @@ def transform_scraped_data(map_from_team_to_player_game_stats):
 
 
 def all_nba_players_gamelog_urls(team_url):
-	# visit www.basketball-reference.com/teams/ATL/2014.html
+	# visit www.basketball-reference.com/teams/ATL/2015.html
 	soup = BeautifulSoup(requests.get(team_url).text)
 	# there's only one table on the page; it contains player names and salaries. 
 	# we know the only hyperlinks in that table are links to the player pages
 	list_of_player_pages = []
-	# TODO: instead of just grabbing all tables, we need to find t
 	for player_a_tag in soup.find(id="all_roster").findAllNext('a'):
 		# we want to return these player urls: 
-		# http://www.basketball-reference.com/players/h/horfoal01/gamelog/2014/
-                list_of_player_pages.append("http://www.basketball-reference.com" + player_a_tag['href'].replace('.html', "") + "/gamelog/2014/")
+		# http://www.basketball-reference.com/players/h/horfoal01/gamelog/2015/
+                player_url = "http://www.basketball-reference.com" + player_a_tag['href'].replace('.html', "") + "/gamelog/2015/"
+                player_name = player_a_tag.string
+                list_of_player_pages.append((player_name, player_url))
 	
 	return list_of_player_pages
 
@@ -78,9 +72,13 @@ def all_nba_teams():
 	# only the children of the table whose class is full_table
 	list_of_team_urls = []
 	for each_a_tag in active_teams_table.findAll('a'):
-		current_year = "2014"
-		list_of_team_urls.append("http://www.basketball-reference.com" + each_a_tag['href'] + current_year + ".html")
+		current_year = "2015"
+                team_name = each_a_tag.string
+                team_url = "http://www.basketball-reference.com" + each_a_tag['href'] + current_year + ".html"
+		list_of_team_urls.append((team_name, team_url))
         print(list_of_team_urls)
 	return list_of_team_urls
 		
-transform_scraped_data(scrape())
+alldata = scrape()
+pickle.dump(alldata, open("nba_player_data.pickle", "wb"))
+transform_scraped_data(alldata)
